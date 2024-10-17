cb0218daa664894c5e42ca1041bd8bf47d1de102
aalara08 <aale080705hgtlrda4@cbtis217.edu.mx>
2024-10-17 10:52:30 +0200

    Version 3 quite comentarios y puse uno solo para explicar mejor

--- suma1/suma1.cpp
+++ suma1/suma1.cpp
@@ -1,15 +1,15 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h> /// PRIMERO DECLARE LAS VARIABLES QUE VOY A OCUPAR LUEGO LAS HICE DOUBBLE PARA DECIMALES LUEGO LAS LEI Y DESPUES HICE EL PROCESO DE LA FORMULA PARA DESPUES CALCULAR LA RAIZ AL CUADRADO Y IMPRIMIR X1 y X2
 using namespace std;
 int main() {
     int X, B, C;
-    double D, x1, x2; // hice double para resultados decimales
+    double D, x1, x2;
     cout<<"soy tuyo, ALCALA LARA EDGAR";
 
-    cin >> X >> B >> C; //lei las variables
+    cin >> X >> B >> C;
 
     D = (B * B) - (4 * X * C);
 
-    x1 = (-B + sqrt(D)) / (2 * X); //en estas 2 puse la formula para calcular la raiz
+    x1 = (-B + sqrt(D)) / (2 * X);
     x2 = (-B - sqrt(D)) / (2 * X);
 
     cout << x1 << " " << x2 << endl;
