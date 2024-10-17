3d87aa38ff43bf05a2674c6aef8510c613af02d0
aalara08 <aale080705hgtlrda4@cbtis217.edu.mx>
2024-10-17 10:48:31 +0200

    Version 2 Agregue comentarios

--- suma1/suma1.cpp
+++ suma1/suma1.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 int main() {
     int X, B, C;
-    double D, x1, x2;
+    double D, x1, x2; // hice double para resultados decimales
+    cout<<"soy tuyo, ALCALA LARA EDGAR";
 
-    cin >> X >> B >> C;
+    cin >> X >> B >> C; //lei las variables
 
     D = (B * B) - (4 * X * C);
 
-    x1 = (-B + sqrt(D)) / (2 * X);
+    x1 = (-B + sqrt(D)) / (2 * X); //en estas 2 puse la formula para calcular la raiz
     x2 = (-B - sqrt(D)) / (2 * X);
 
     cout << x1 << " " << x2 << endl;
